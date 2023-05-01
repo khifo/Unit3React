f99145dcb1e8a47f0001ca4b9c40829bdc4f8a97
khifo <khifo09@yahoo.com>
2023-05-01 10:13:29 -0600

    init commit

---
+++ react-proj-3/starter/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Devmountain Eatery</h2>
+      <nav>
+        <Link to="">
+          <button>Home</button>
+          <button>Add Recipe</button>
+      </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
